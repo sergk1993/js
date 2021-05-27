8921547e80e6e7bf82a6687d3e3319e8e5d69228
sergk1993 <81139100+sergk1993@users.noreply.github.com>
2021-05-27 08:32:06 +0600

    learn js

---
+++ js/script.js
@@ -0,0 +1,85 @@
+// alert('Привет!');
+// let one = prompt('Как тебя зовут?');
+// let two = prompt('Как зовут твоего друга?');
+// let three = +prompt('Сколько у тебя яблок?');
+// let four = +prompt('Сколько яблок у твоего друга?');
+// let multiplay = three + four
+// // alert(`Значит ${one} и ${two} имеют ${three + four} ` );
+// // let answer = 'Значит ' + one + ' и ' + two + ' имеют ' + multiplay + ' яблок!';
+// let answer = `Значит ${one} и ${two} имеют ${multiplay} яблок!`
+// alert(answer);    
+
+
+// let serg = ('привет');
+// serg = 'что тебе надо '
+// alert(serg);
+
+// let cat = ('7')
+// alert(`Количество жизней у кошки ${cat}`)
+
+// let cat = 'Sam'; 
+// let dog = cat; 
+// alert(cat); // ??? sam
+// alert(dog); // ??? sam
+// cat = 'Murzik'; 
+// alert(cat); // ??? 
+// alert(dog); // ??? 
+
+// console.log(123);
+// console.log('123');
+
+// let question = prompt('Введите вопрос')
+// let name = prompt(question);
+
+
+// let question = prompt('Введите вопрос')
+// console.log(confirm(`Строка ${question}`));
+// let name = prompt(question);
+
+// const result = 999 ** 999;
+// console.log(result);
+
+// console.log('1' && '2' || '3');
+// console.log(2 * 3 >= 9 / 2 || 4 % 0 && 12 == 3 * 4 || null);
+
+// let num = 5;
+// // num = 10;
+// if (num == 5)
+//  {console.log('Хай!')
+// } else {
+//     console.log('нет')
+// }
+
+
+
+// let ears = prompt('Введите число');
+// switch (ears) {
+//     case '1':
+//         alert( '7а"' );
+//         console.log('7a');
+//         break;
+//         case 'света леша':
+//             alert( '7б"' );
+//             break;
+//   default:
+//     alert( '-1' );
+//     console.log('-1');
+// }
+
+
+// for (count = 10; count; count = count -1){
+//     console.log(count);
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// for (let i = 0; i <= 20; i++){
+//     if(i % 2 == 0){
+//         console.log(i)
+//     }
+// }
+
+
+
